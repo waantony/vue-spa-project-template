2413c0e5e4fa4ab86d2509bfe434503584cfa789
waantony <wanganantony@gmail.com>
2020-09-30 11:13:46 +0800

    完善项目配置，生产环境依赖全部加入 externals，运行时从 CDN 引入

--- babel.config.js
+++ babel.config.js
@@ -3,14 +3,14 @@ module.exports = {
     '@vue/cli-plugin-babel/preset',
   ],
   plugins: [
-    [
-      'import',
-      {
-        libraryName: 'vant',
-        libraryDirectory: 'es',
-        style: true,
-      },
-      'vant',
-    ],
+    // [
+    //   'import',
+    //   {
+    //     libraryName: 'vant',
+    //     libraryDirectory: 'es',
+    //     style: true,
+    //   },
+    //   'vant',
+    // ],
   ],
 }
--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'normalize.css'
 import '@/styles/index.scss'
+import 'vant/lib/index.css'
 
 // 全部引入 vant 组件
-// import Vant from 'vant'
-// import { Button } from 'vant'
-// import 'vant/lib/index.css'
-// Vue.use(Button)
+import Vant from 'vant'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
