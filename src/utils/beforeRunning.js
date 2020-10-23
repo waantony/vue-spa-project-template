4451357af161e28c92b5159ad0dbfe8b8aadb29d
waantony <wanganantony@gmail.com>
2020-10-23 17:24:26 +0800

    修改 beforeRunning.js

--- src/utils/beforeRunning.js
+++ src/utils/beforeRunning.js
@@ -1,5 +1,15 @@
 // 程序一开始时需要先执行的操作
 
-import { removeSomeQuery } from '@/utils/url'
+import { querystringToObj, removeSomeQuery } from '@/utils/url'
+import { setSession } from '@/utils/storage'
 
-location.replace(removeSomeQuery(['token', 'user_id']))
+const queryKeyList = ['token', 'user_id']
+const queryObj = querystringToObj(location.href)
+queryKeyList.forEach(key => {
+  const value = queryObj[key]
+  value && setSession(key, value)
+})
+if (queryKeyList.some(key => queryObj[key])) {
+  const targetUrl = removeSomeQuery(queryKeyList)
+  location.replace(targetUrl)
+}
