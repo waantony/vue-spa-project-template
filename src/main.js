554d6d950ead9a245928c47f2dcd49ba64b486c9
waantony <wanganantony@gmail.com>
2020-12-24 14:39:24 +0800

    main.js 引入顺序调整

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import '@/styles/index.scss'
 import 'vant/lib/index.css'
 
