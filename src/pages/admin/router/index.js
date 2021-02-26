e84c034c01849d358fa043d667ce896cee689a3f
waantony <wanganantony@gmail.com>
2021-02-26 10:17:03 +0800

    修改 admin 配置

--- src/pages/admin/router/index.js
+++ src/pages/admin/router/index.js
@@ -1,20 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routerGuards from './routerGuards'
-// import Home from '../views/home'
 
-// 路由模块
-import controlledRoutes from './modules/controlledRoutes'
+// 非侧边栏路由模块
 import publicRoutes from './modules/publicRoutes'
 import notMatchedRoutes from './modules/notMatchedRoutes'
-import LayoutRoutes from './modules/LayoutRoutes'
+
+// 无权限的侧边栏路由
+import notControlledSidebarRoutes from './sidebar'
+
 Vue.use(VueRouter)
 
+// 初始化的路由列表
 const routes = [
-  ...publicRoutes,
-  ...controlledRoutes,
-  ...LayoutRoutes,
-  ...notMatchedRoutes,
+  ...notControlledSidebarRoutes, // 无权限的侧边栏路由
+  ...publicRoutes, // 无登录，无权限的开放路由
+  ...notMatchedRoutes, // 不匹配路由
 ]
 
 // 实例化 router
--- src/pages/admin/router/sidebar/index.js
+++ src/pages/admin/router/sidebar/index.js
@@ -1,5 +1,6 @@
 import Layout from '@/routerComponents/Layout'
 import notControlledRoutes from './notControlledRoutes'
+// import controlledRoutes from './controlledRoutes'
 
 // 需要 Layout 布局的路由，全部作为 children 使用
 const children = [
